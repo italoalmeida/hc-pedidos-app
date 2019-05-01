e303950e0d9d3e47a887b492d6e8ecc8092d272b
Ítalo Almeida <italo.almeida04@gmail.com>
2019-04-30 23:43:09 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pedidos de Pratos - backend
+Aplicação para pedidos de pratos via API RESTful
---
+++ src/main/java/com/italoalmeida/hc/HcPedidosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.italoalmeida.hc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcPedidosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcPedidosBackendApplication.class, args);
+	}
+
+}
